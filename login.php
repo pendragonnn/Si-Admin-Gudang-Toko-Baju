9450109974cc88762d7d9ee3e00bda1e888e67cd
Wisnu Andika <wisnuandika104@gmail.com>
2023-06-04 20:49:57 +0700

    Memodifikasi halaman login

--- login.php
+++ login.php
@@ -33,35 +33,42 @@ if(isset($_POST['login'])){
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Siadmin | Login</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <title>Login | Si Admin</title>
   <link rel="stylesheet" href="./assets/css/login.css?v=<?php echo time(); ?>">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-  <div class="containe-login p-0 mt-5" >
-    <div class="containe-login-2 p-0 m-5" >
-      <div class="containe-login-3 p-0 m-5" >
-        <h5 class="card-header text-center bg-primary text-light">Login Admin</h5>
-          <form class="p-3"action="" method="post">
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" name="username" class="form-control" id="username">
+  <div class="wrapper">
+    <div class="container main">
+      <div class="row">
+        <div class="col-md-6 side-image">
+          <img src="assets/images/login.jpg" alt="">
+        </div>
+        <div class="col-md-6 right">
+          <form method="post">
+            <div class="input-box">
+              <header>Admin Login</header>
+              <div class="input-field">
+                <input type="text" name="username" class="input" id="email" required autocomplete="off">
+                <label for="email">Username</label>
+              </div>
+              <div class="input-field">
+                <input type="password" name="password" class="input" id="password" required>
+                <label for="password">Password</label>
+              </div>
+              <div class="input-field">
+                <input type="submit" name="login" class="submit" value="Login">
+                <?php 
+                  if(isset($error)) :?>
+                  <script>alert('Username / password salah')</script>
+                  <?php endif;
+                ?>
+              </div>
             </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" name="password" class="form-control" id="password">
-            </div>
-              <button type="submit" name="login" class="btn btn-primary">Login</button>
-              <?php 
-                if(isset($error)) :?>
-                <script>alert('Username / password salah')</script>
-                <?php endif;
-              ?>
           </form>
         </div>
       </div>
     </div>
   </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
